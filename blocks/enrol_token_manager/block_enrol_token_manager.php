a6f4a190c682818ced14ee1bff3612b3431c3dd9
Tim St.Clair <tim.stclair@gmail.com>
2017-03-13 11:35:47 +1100

    Code formatting

--- blocks/enrol_token_manager/block_enrol_token_manager.php
+++ blocks/enrol_token_manager/block_enrol_token_manager.php
@@ -1,50 +1,54 @@
 <?php
 class block_enrol_token_manager extends block_base {
 
-    function init() {
-        $this->title = get_string('pluginname', 'block_enrol_token_manager');
-    }
+	function init() {
+		$this->title = get_string('pluginname', 'block_enrol_token_manager');
+	}
 
-    function applicable_formats() {
-        return array('site' => true);
-    }
+	function applicable_formats() {
+		return array('site' => true);
+	}
 
-    function get_content() {
+	function can_hide_show_instance() {
+		return true;
+	}
 
-        if ($this->content !== NULL) {
-            return $this->content;
-        }
+	function get_content() {
 
-        $context = context_system::instance();
+		if ($this->content !== NULL) {
+			return $this->content;
+		}
 
-        $this->content = new stdClass();
-        $this->content->footer = '';
-        $this->content->text = '';
-        $content = array();
+		$context = context_system::instance();
 
-        $canview = (has_capability('block/enrol_token_manager:viewtokens', $context) === true);
-        $cancreate = (has_capability('block/enrol_token_manager:createtokens', $context) === true);
-        $canrevoke = (has_capability('block/enrol_token_manager:revoketokens', $context) === true);
+		$this->content = new stdClass();
+		$this->content->footer = '';
+		$this->content->text = '';
+		$content = array();
 
-        // view tokens link
-        if ($canview) {
-            $content[] = html_writer::link(new moodle_url('/blocks/enrol_token_manager/viewrevoke_tokens.php'), get_string('linkTextViewTokens', 'block_enrol_token_manager'));
-        }
+		$canview = (has_capability('block/enrol_token_manager:viewtokens', $context) === true);
+		$cancreate = (has_capability('block/enrol_token_manager:createtokens', $context) === true);
+		$canrevoke = (has_capability('block/enrol_token_manager:revoketokens', $context) === true);
 
-        // create tokens link
-        if ($cancreate) {
-            $content[] = html_writer::link(new moodle_url('/blocks/enrol_token_manager/create_tokens.php'), get_string('linkTextCreateTokens', 'block_enrol_token_manager'));
-        }
+		// view tokens link
+		if ($canview) {
+			$content[] = html_writer::link(new moodle_url('/blocks/enrol_token_manager/viewrevoke_tokens.php'), get_string('linkTextViewTokens', 'block_enrol_token_manager'));
+		}
 
-        // revoke tokens link
-        if ($canrevoke) {
-            $content[] = html_writer::link(new moodle_url('/blocks/enrol_token_manager/viewrevoke_tokens.php'), get_string('linkTextRevokeTokens', 'block_enrol_token_manager'));
-        }
+		// create tokens link
+		if ($cancreate) {
+			$content[] = html_writer::link(new moodle_url('/blocks/enrol_token_manager/create_tokens.php'), get_string('linkTextCreateTokens', 'block_enrol_token_manager'));
+		}
 
-        if ($cancreate || $canrevoke || $canview) {
-          $this->content->text = html_writer::alist($content);
-        }
+		// revoke tokens link
+		if ($canrevoke) {
+			$content[] = html_writer::link(new moodle_url('/blocks/enrol_token_manager/viewrevoke_tokens.php'), get_string('linkTextRevokeTokens', 'block_enrol_token_manager'));
+		}
 
-        return $this->content;
-    }
+		if ($cancreate || $canrevoke || $canview) {
+		  $this->content->text = html_writer::alist($content);
+		}
+
+		return $this->content;
+	}
 }
