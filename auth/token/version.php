51bcc8d9362c421befbadaf8e0b3ae0a95d28b4c
Tim St.Clair <tim.stclair@gmail.com>
2017-03-13 11:33:38 +1100

    bumped version

--- auth/token/version.php
+++ auth/token/version.php
@@ -1,6 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015121100;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2017031300;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013050100;        // Requires this Moodle version
 $plugin->component = 'auth_token';       // Full name of the plugin (used for diagnostics)
