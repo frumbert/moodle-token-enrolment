9bf0946c0635ec6350c80125542ea3cbde6bd23d
Tim St.Clair <tim.stclair@gmail.com>
2017-03-13 11:36:09 +1100

    Fixed wording

--- blocks/enrol_token_manager/lang/en/block_enrol_token_manager.php
+++ blocks/enrol_token_manager/lang/en/block_enrol_token_manager.php
@@ -1,12 +1,6 @@
 <?php
 
-// $string['cohortinstructions'] = '<p>A \'cohort\' is a way of grouping people who use tokens. You can create any grouping types that you\'d like.</p>' .
-//                                '<p>For example, you may create a cohort such as \'BHP\', \'Main Office,\' or \'Give-aways.\'</p>';
-
-
 $string['createtokens'] = 'Create Tokens';
-$string['emailaddressprompt'] = 'Email address to send the tokens to';
-$string['emailsubject'] = 'Pre-paid Coupons for your online learning';
 
 $string['enrol_token_manager:addinstance'] = 'Add a new login block';
 $string['enrol_token_manager:createtokens'] = 'Create Enrolment Tokens';
@@ -19,10 +13,10 @@ $string['linkTextViewTokens'] = 'View enrolment token details';
 $string['nopagepermission'] = 'You do not have suitable persmissions to view the page';
 
 $string['noticetext'] = '<p>Hello,</p>' .
-                        '<p>Please find below {$a->tokennumber} Pre-Paid Coupon Code{$a->tokennumberplural} that can be used to enrol into the <strong>{$a->coursename}</strong> on-line course.</p>' .
-                        '<p>Simply go to <a href="{$a->wwwroot}">{$a->wwwroot}</a> and use your Coupon Code to enrol into and complete your course.</p>' .
+                        '<p>Please find below {$a->tokennumber} token code{$a->tokennumberplural} that can be used to enrol into the <strong>{$a->coursename}</strong> course.</p>' .
+                        '<p>Simply go to <a href="{$a->wwwroot}">{$a->wwwroot}</a> and use your token to enrol into and complete your course.</p>' .
                         '<p>Your Coupon Code{$a->tokennumberplural}: <strong>{$a->tokens}</strong></p>' .
-                        '<p>PS. Each Coupon Code is valid for {$a->seatspertoken} enrolment{$a->seatspertokenplural} into the course.</p>' .
+                        '<p>Each token is valid for {$a->seatspertoken} enrolment{$a->seatspertokenplural} into the course.</p>' .
                         '<p>Regards,<br>' .
                         '{$a->adminsignoff}</p>';
 
@@ -37,6 +31,7 @@ $string['promptcourse'] = 'Generate tokens for this course';
 $string['promptcourse_help'] = 'The course you select must have the <em>manual enrolment</em> method enabled';
 
 $string['cohort_selection'] = 'Cohort container';
+$string['cohortor'] = 'OR';
 $string['cohortor_help'] = 'Users who enrol using a token generated on this page will be added to a Cohort; you can choose an existing cohort, or create a new one.';
 
 $string['token_generation'] = 'Token generation';
@@ -55,6 +50,7 @@ $string['promptexpirydate'] = 'Token expiry date (if enabled).';
 
 $string['promptfiltertoken'] = 'Token code';
 $string['tokeninstructions_help'] = 'You can use \'*\' to search for any characters (e.g. <em>token*</em> will match token1, token12, token_bob, etc), and \'?\' to represent a single unknown character (e.g. <em>to?en</em> will match token, tojen, to6en, etc). You can use both together (e.g. <em>to?en*</em>)';
+$string['promptfiltertoken_help'] = $string['tokeninstructions_help'];
 
 $string['promptfiltercohort'] = 'Cohort (group)';
 $string['promptfiltercourse'] = 'Course';
@@ -63,14 +59,52 @@ $string['promptprefix'] = 'Short token prefix';
 $string['promptprefix_help'] = 'Generated tokens will begin with the text you enter here (default: blank)';
 
 $string['promptseats'] = 'Enrolments for each Token';
-$string['promptseats_help'] = 'How many times each token can be used for an enrolment (suggested: 1; must be more than 0 and less than 201)';
+$string['promptseats_help'] = 'How many times each token can be used for an enrolment (suggested: 1; must be more than 0 and less than 501)';
 
 $string['prompttokennum'] = 'Number of tokens to produce';
 $string['prompttokennum_help'] = 'Enter the number of tokens you want to generate using these details (example: 10; must be more than 0 and less than 501)';
 
 $string['revoketokens'] = 'Revoke Selected Tokens';
-$string['seatsoutofrange'] = 'The number of enrolments per Token cannnot be negative or larger than 200';
-$string['tokensoutofrange'] = 'The number of Tokens to produce cannnot be negative or larger than 500';
+$string['seatsoutofrange'] = 'The number of enrolments per Token cannnot be zero, negative or larger than 500';
+$string['tokensoutofrange'] = 'The number of Tokens to produce cannnot be zero, negative or larger than 500';
 $string['viewtokens'] = 'View Tokens';
 
 $string['token_delete_error'] = 'There was a problem revoking the tokens - please double-check that they have in fact been deleted by re-searching for them';
+
+$string['emailaddressprompt'] = 'Send tokens to this email';
+
+$string['mailsubject'] = 'Mail subject';
+$string['mailsubject_help'] = 'Subject of email to be sent after tokens are generated';
+
+$string['mailbody'] = 'Mail body';
+$string['mailbody_help'] = '<p>Body of email to be sent after tokens are generated. You can use the following merge fields:</p>
+*{coursename}* - the full name of the course
+
+*{tokennumber}* - the number of tokens that were generated
+
+*{tokennumberplural}* - the letter "s" if there is more than one token
+
+*{seatspertoken}* - the number of seats that were generated
+
+*{seatspertokenplural}* - the letter "s" if there is more than one seat
+
+*{wwwroot}* - the url of this server up until (but not including) the trailing slash (e.g. http://foo.com but not http://foo.com/ )
+
+*{tokens}* - a comma seperated list of the tokens (or a single token value, if there is only one)
+
+*{adminsignoff}* - the site-wide standard email sign-off (configured elsewhere)
+
+*{emailaddress}* - the email address you are sending to
+
+HTML / Markdown is ok.
+';
+
+$string['mailsubject_default'] = 'Your registration token(s) for Moodle';
+$string['mailbody_default'] = 'Hello,
+
+Please find below {tokennumber} token(s) that can be used to enrol onto Moodle. Use the link below to open the registration page:
+
+{wwwroot}/auth/token/login.php?token={tokens}
+
+Cheers,
+{adminsignoff}';
